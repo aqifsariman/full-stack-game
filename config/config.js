114c2c1a5d7b2dcf41bd1f1753a37d1197836e6b
aqifsariman <aqifsariman@gmail.com>
2022-06-09 15:02:06 +0800

    First Commit

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    username: 'aqifsariman',
+    password: null,
+    database: 'full_stack_game_development',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+};
